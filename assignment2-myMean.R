455254bdf17a08f26d4f1961dcc10f5c198ab485
shanzay28 <shanzay282@gmail.com>
2025-09-06 22:20:03 -0400

    Update assignment2-myMean.R

--- assignment2-myMean.R
+++ assignment2-myMean.R
@@ -1,7 +1,6 @@
 assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
 
-# Broken version (will cause an error)
-myMean <- function(assignment2) {
+#myMean <- function(assignment2) {
   return(sum(assignment) / length(someData))
 }
 myMean(assignment2)
